9ac8f3678a0ecf05bb78da5b3bff77750251973f
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-09-18 13:24:22 -0700

    Completed Activity

--- phython-practice-4.py
+++ phython-practice-4.py
@@ -29,4 +29,23 @@ print(num_within(3, 2, 4))
 print(num_within(3, 1, 3))
 print(num_within(10, 2, 5))
 
+#print out the first 5 rows of Pascal's triangle
+def pascal(n):
+    if n <= 0:
+        print("Number of rows should be a positive integer.")
+        return
 
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            value = triangle[i-1][j-1] + triangle[i-1][j]
+            row.append(value)
+        row.append(1)
+        triangle.append(row)
+
+    # code to make the triangle
+    for row in triangle:
+        print(' '.join(map(str, row)).center(n*3))
+
+pascal(5)
